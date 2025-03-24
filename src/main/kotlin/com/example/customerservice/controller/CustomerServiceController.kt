41a3ff5b93ae59a2403f9bad8ddcaad5b2f932d8
rx <76666903+rx10@users.noreply.github.com>
2025-03-24 17:39:57 +0530

    change cors origin domain

--- src/main/kotlin/com/example/customerservice/controller/CustomerServiceController.kt
+++ src/main/kotlin/com/example/customerservice/controller/CustomerServiceController.kt
@@ -5,7 +5,7 @@ import com.example.customerservice.service.CustomerService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin(origins = ["http://localhost:4200"])
+@CrossOrigin(origins = ["https://vercelfrontend-zeta.vercel.app"])
 @RestController
 @RequestMapping("/api/customers")
 class CustomerController(private val service: CustomerService) {
